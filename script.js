1fd0cb5be03fa5261e97bd9fbade00d02825dba1
Ladyriv <leidy.riveraf@outlook.com>
2022-11-11 22:54:46 -0500

    V.01

---
+++ script.js
@@ -0,0 +1,35 @@
+//Boton cambio modo
+
+const input = document.getElementsByClassName("calculator");
+const botonSwitch = document.getElementById("check");
+
+botonSwitch.addEventListener("click", function(){
+    if(botonSwitch.checked){
+        console.log("dark")
+    }
+    else{
+        console.log("light") 
+    }
+});
+
+const toggleThemeMode = () => {
+    calculator.classList.toggle("dark");
+};
+calculator.onchange = toggleThemeMode;
+
+//funcionamiento calculadora
+function display(val) {
+    document.getElementById('result').value += val;
+}
+
+function clearScreen() {
+    document.getElementById('result').value = "";
+}
+
+function solve() {
+    let x = document.getElementById('result').value;
+    let y = eval(x);
+    document.getElementById('result').value = y;
+    return y;
+}
+
